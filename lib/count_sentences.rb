2f6ce9371ac704d22a7552550dd5052de1473e6f
Blake Creasser <blakecreasser@aggienetwork.com>
2023-02-22 15:34:53 -0600

    commit tree 394c384aa4ef1ed29c6cdb42e445a8d9db0ec6fa

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    #split the string on the period, then we can check the length of that index in the arr and if greater than 1 push it to the new array
+    sentences = 0
+    punc = [".", '?', '!']
+    self.chars.each.with_index do |char, index|
+      if punc.include?(char) && !punc.include?(self[index + 1])
+        sentences += 1
+      end
+    end
+    sentences
+    #self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.size
+    
   end
 end
